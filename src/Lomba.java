f8dac58f08789f15e793f640a11bc11362de46c7
Deviramadhia82 <62375133+Deviramadhia82@users.noreply.github.com>
2021-01-25 17:47:21 +0700

    Update Lomba.java

--- src/Lomba.java
+++ src/Lomba.java
@@ -15,15 +15,37 @@ import javafx.beans.property.StringProperty;
  */
 public abstract class Lomba {
     private IntegerProperty id_lomba;
-    private StringProperty nama_lomba;
+    private StringProperty nama;
     private IntegerProperty Harga;
     
-    public Lomba(String nama_lomba){
+    public Lomba(String nama_lomba){ //constructor
         this.id_lomba = new SimpleIntegerProperty ();
-        this.nama_lomba = new SimpleStringProperty (nama_lomba);
+        this.nama = new SimpleStringProperty (nama_lomba);
+        this.Harga = new SimpleIntegerProperty();
+    }
+    
+    public void setID(int id){
+        this.id_lomba.set(id);
+    }
+    
+    public void setHarga(int hrg){
+        this.Harga.set(hrg);
+    }
+    
+    public int getID(){
+        return this.id_lomba.get();
+    }
+    
+    public String getNama(){
+        return this.nama.get();
+    }
+    
+    public int getHarga(){
+        return this.Harga.get();
     }
     
     public abstract StringProperty NamaProperty();
     public abstract IntegerProperty HargaProperty();
+    
     
 }
