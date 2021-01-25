21537b3de56c70e176c0e309c13667e3900d06ec
Hanisendy <62375293+Hanisendy@users.noreply.github.com>
2021-01-25 17:33:53 +0700

    Update MobileLegend.java

--- src/MobileLegend.java
+++ src/MobileLegend.java
@@ -13,24 +13,28 @@ import javafx.beans.property.StringProperty;
  *
  * @author ACER
  */
+//subclass lomba
 public class MobileLegend extends Lomba {
-    private StringProperty nama;
-    private IntegerProperty Harga;
+    IntegerProperty id;
+    StringProperty nama;
+    IntegerProperty Harga;
     
     public MobileLegend(String nama) {
         super(nama);
+        super.setID(2);
+        super.setHarga(100000);
         this.nama = new SimpleStringProperty (nama);
         this.Harga = new SimpleIntegerProperty(100000);
-    }
+    } //constructor
     
     @Override
     public StringProperty NamaProperty() {
         return this.nama;
-    }
+    } //inheritance
 
     @Override
     public IntegerProperty HargaProperty() {
         return this.Harga;
     }
-    
+
 }
