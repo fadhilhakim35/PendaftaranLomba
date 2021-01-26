b02942a898ce76b2bb41ffb75ca7c70939de4daf
Hanisendy <62375293+Hanisendy@users.noreply.github.com>
2021-01-26 19:23:40 +0700

    Create PUBG.java

---
+++ src/pendaftaranlomba/PUBG.java
@@ -0,0 +1,40 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pendaftaranlomba;
+
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+/**
+ *
+ * @author ACER
+ */
+
+public class PUBG extends Lomba {
+    StringProperty nama;
+    IntegerProperty Harga;
+    
+    public PUBG (String nama) {
+        super(nama);
+        super.setID(1);
+        this.nama = new SimpleStringProperty (nama);
+        this.Harga = new SimpleIntegerProperty (150000);
+        super.setHarga(this.Harga.get());
+    }
+    
+    @Override
+    public StringProperty NamaProperty() {
+        return nama;
+    }
+
+    @Override
+    public IntegerProperty HargaProperty() {
+        return Harga;
+    }
+
+    
+}
